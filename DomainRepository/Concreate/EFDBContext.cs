b1b603a9280f24c66cb92905761899c16501f4af
aissa <aissa@DESKTOP-EJIEBQO>
2019-10-25 10:48:45 -0400

    test

---
+++ DomainRepository/Concreate/EFDBContext.cs
@@ -0,0 +1,15 @@
+﻿using DomainRepository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainRepository.Concreate
+{
+    public class EFDBContext :DbContext
+    {
+        DbSet<Product> Products;
+    }
+}
